28abd872053cf32057b97ff90fa4229d32c0bd4b
hasnainbilgrami <hrbilgra@gmail.com>
2015-11-21 23:31:03 -0600

    v1.3

--- foodpicker/foodpicker/Controllers/HomeController.cs
+++ foodpicker/foodpicker/Controllers/HomeController.cs
@@ -14,8 +14,8 @@ namespace foodpicker.Controllers
 			var mvcName = typeof(Controller).Assembly.GetName ();
 			var isMono = Type.GetType ("Mono.Runtime") != null;
 
-			ViewData ["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
-			ViewData ["Runtime"] = isMono ? "Mono" : ".NET";
+		//	ViewData ["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+		//	ViewData ["Runtime"] = isMono ? "Mono" : ".NET";
 
 			return View ();
 		}
